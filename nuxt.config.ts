5b139063fdfcfd32174d2ce054d67225708509cf
MatthewH-PE <matth@peinfo.com>
2025-07-09 22:45:15 -0500

    Added DockerFile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  nitro: {
+    preset: 'node-server' // Ensure this is set for Node.js server deployment
+  },
   devtools: { enabled: true },
   modules: [
     'vuetify-nuxt-module'
